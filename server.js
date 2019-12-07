0f8f34cd583b7eda2cdaefd841c86593ce1d5a29
Priyansh <mr.priyanshgaharana@gmail.com>
2019-12-07 12:44:11 +0530

    heruku

--- server.js
+++ server.js
@@ -87,6 +87,6 @@ app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
 
 */
 
-app.listen(3000,()=>{
-	console.log('app is running');
+app.listen(process.env.PORT || 3000,()=>{
+	console.log(`app is running ${process.env.PORT}`);
 });
